5a5b61ecfd6419d371c367cb55778a902b51885c
jesper-helin <helin.jesper@gmail.com>
2022-06-05 13:31:43 +0300

    Home page

--- space-tourism-website/src/pages/Home/index.js
+++ space-tourism-website/src/pages/Home/index.js
@@ -3,7 +3,19 @@ import './Home.scss';
 const Home = () => {
    return (
       <div className="home">
-         {/* <h1>Home</h1> */}
+         <div className="home-introduction-text">
+            <h5>So, you want to travel to</h5>
+            <h1>Space</h1>
+            <p>
+               Let’s face it; if you want to go to space, you might as well
+               genuinely go to outer space and not hover kind of on the edge of
+               it. Well sit back, and relax because we’ll give you a truly out
+               of this world experience!
+            </p>
+         </div>
+         <div className="home-explore">
+            <h4>Explore</h4>
+         </div>
       </div>
    );
 };
